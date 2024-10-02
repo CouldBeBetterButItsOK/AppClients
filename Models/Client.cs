45d90044d2c844027b2b20c8faed23257d20a99c
CouldBeBetterButItsOk <francescxavier.sane@uvic.cat>
2024-10-02 18:08:32 +0200

    Primer commit

---
+++ Models/Client.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_MVVM_SPA_Template.Models
+{
+    class Client
+    {
+        public string Name { get; set; }
+        public string DniNif { get; set; }
+        public string Code { get; set; }
+        public bool Profesional { get; set; }
+        public int Discount { get; set; } // Descuento en porcentaje
+        public DateTime RegistrationDate { get; set; } // Fecha de alta
+        public double TotalAnualSells { get; set; } // Ventas totales anuales
+
+        // Constructor
+        public Client(string name, string dniNif, string code, bool profesional, int discount, DateTime registrationDate, double anualTotalSells)
+        {
+            Name = name;
+            DniNif = dniNif;
+            Code = code;
+            Profesional = profesional;
+            Discount = discount;
+            RegistrationDate = registrationDate;
+            TotalAnualSells = anualTotalSells;
+        }
+        public Client() { }
+
+    }
+}
---
+++ ViewModels/ClientViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using WPF_MVVM_SPA_Template.Models;
+
+namespace WPF_MVVM_SPA_Template.ViewModels
+{
+    class ClientViewModel : INotifyPropertyChanged
+    {
+        private readonly MainViewModel _mainViewModel;
+        public ObservableCollection<Client>Clients{ get; set; } = new ObservableCollection<Client>();
+
+        private Client? _selectedClient;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string? name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public Client? SelectedClient
+        {
+            get { return _selectedClient; }
+            set { _selectedClient = value; OnPropertyChanged(); }
+        }
+        public RelayCommand AddClientCommand { get; set; }
+        public RelayCommand DelClientCommand { get; set; }
+
+        public ClientViewModel(MainViewModel mainViewModel)
+        {
+            _mainViewModel = mainViewModel;
+            // Carreguem estudiants a memòria mode de prova
+            Clients.Add(new Client { Code = "C001", Name = "David", DniNif = "12345678A", Profesional = true, Discount = 10, RegistrationDate = DateTime.Now, TotalAnualSells = 1500.00 });
+            Clients.Add(new Client { Code = "C002", Name = "Jordi", DniNif = "87654321B", Profesional = false, Discount = 5, RegistrationDate = DateTime.Now, TotalAnualSells = 1200.00 });
+            // Inicialitzem els diferents commands disponibles (accions)
+            AddClientCommand = new RelayCommand(x => AddClient());
+            DelClientCommand = new RelayCommand(x => DelClient());
+        }
+        private void AddClient()
+        {
+            Clients.Add(new Client { Code = "C002", Name = "Jordi", DniNif = "87654321B", Profesional = false, Discount = 5, RegistrationDate = DateTime.Now, TotalAnualSells = 1200 });
+        }
+
+        private void DelClient()
+        {
+            if (SelectedClient != null)
+                Clients.Remove(SelectedClient);
+        }
+        
+    }
+       
+
+}
